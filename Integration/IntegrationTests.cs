42149e0d919908dbbf188b79a84eec2b7eaf8620
Martin Stimpfl <martin.stimpfl@miba.com>
2019-03-13 09:32:57 +0100

    restructured tests - added integration test

--- Integration/IntegrationTests.cs
+++ Integration/IntegrationTests.cs
@@ -9,28 +9,42 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Net;
 using AccountOwnerServer;
+using Newtonsoft.Json;
+using Entities.Models;
 
 namespace Integration
 {
     public class IntegrationTests
     {
-        [Fact]
-        public async Task GetAllOwners_ReturnsAListOfOwners()
-        {
-            // Arrange
-            var server = new TestServer(new WebHostBuilder()
-                .UseConfiguration(new ConfigurationBuilder()
-                    .AddJsonFile("appsettings.json")
-                    .Build())
-                .UseStartup<Startup>());
-            var client = server.CreateClient();
+        private readonly TestContext _context;
 
+        public IntegrationTests()
+        {
+            _context = new TestContext();
+        }
+
+        [Fact]
+        public async Task GetAllOwners_ReturnsOkResponse()
+        {
             // Act
-            var response = await client.GetAsync("/api/owner");
+            var response = await _context.Client.GetAsync("/api/owner");
             response.EnsureSuccessStatusCode();
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAllOwners_ReturnsAListOfOwners()
+        {
+        // Act
+		var response = await _context.Client.GetAsync("/api/owner");
+		response.EnsureSuccessStatusCode();
+		var responseString = await response.Content.ReadAsStringAsync();
+		var owners = JsonConvert.DeserializeObject<List<Owner>>(responseString);
+ 
+		// Assert
+		Assert.NotEmpty(owners);
         }
     }
 }
